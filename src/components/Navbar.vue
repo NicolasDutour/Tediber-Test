074a48dd30d382b9c5f46e2bb22bc2a107ca01f6
NicolasDutour <dutourn99@gmail.com>
2019-04-06 15:03:15 +0200

    Add Vuetify

---
+++ src/components/Navbar.vue
@@ -0,0 +1,33 @@
+<template>
+  <nav>
+    <v-toolbar color="white" flat>
+      <v-btn flat to="/">logo</v-btn>
+
+      <v-btn flat to="/products">nos produits</v-btn>
+
+      <v-btn flat to="/concept">le concept</v-btn>
+
+      <v-btn flat to="/testimonials">avis</v-btn>
+
+      <v-spacer></v-spacer>
+
+      <v-btn flat to="/">logo</v-btn>
+
+      <v-spacer></v-spacer>
+
+      <v-btn flat to="/pubtv">pub tv</v-btn>
+
+      <v-btn flat to="/blog">blog</v-btn>
+
+      <v-btn flat to="/myaccount">mon compte</v-btn>
+
+      <v-btn flat>0186954710</v-btn>
+
+      <v-btn flat to="/cart">cart</v-btn>
+    </v-toolbar>
+  </nav>
+</template>
+
+<script>
+export default {};
+</script>
--- src/router.js
+++ src/router.js
@@ -1,26 +1,64 @@
-import Vue from "vue";
-import Router from "vue-router";
-import Home from "./views/Home.vue";
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from './views/Home.vue'
+import Products from './views/Products.vue'
+import Concept from './views/Concept.vue'
+import Testimonials from './views/Testimonials.vue'
+import PubTv from './views/PubTv.vue'
+import Blog from './views/Blog.vue'
+import MyAccount from './views/MyAccount.vue'
+import Cart from './views/Cart.vue'
+import Presse from './views/Presse.vue'
 
-Vue.use(Router);
+Vue.use(Router)
 
 export default new Router({
-  mode: "history",
-  base: process.env.BASE_URL,
-  routes: [
-    {
-      path: "/",
-      name: "home",
-      component: Home
-    },
-    {
-      path: "/about",
-      name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () =>
-        import(/* webpackChunkName: "about" */ "./views/About.vue")
-    }
-  ]
-});
+    mode: 'history',
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: Home
+        },
+        {
+            path: '/products',
+            name: 'products',
+            component: Products
+        },
+        {
+            path: '/concept',
+            name: 'concept',
+            component: Concept
+        },
+        {
+            path: '/testimonials',
+            name: 'testimonials',
+            component: Testimonials
+        },
+        {
+            path: '/pubtv',
+            name: 'pubtv',
+            component: PubTv
+        },
+        {
+            path: '/blog',
+            name: 'blog',
+            component: Blog
+        },
+        {
+            path: '/myaccount',
+            name: 'myaccount',
+            component: MyAccount
+        },
+        {
+            path: '/cart',
+            name: 'cart',
+            component: Cart
+        },
+        {
+            path: '/presse',
+            name: 'presse',
+            component: Presse
+        }
+    ]
+})
